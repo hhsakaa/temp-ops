7d825ff8b8a1014a43407344c141e2cb4917e0e0
Akash Raghav <94389454+hhsakaa@users.noreply.github.com>
2025-03-30 21:45:14 +0530

    SDK-workflow

---
+++ workflows.py
@@ -0,0 +1,26 @@
+from temporalio import workflow
+
+@workflow.defn
+class MyWorkflow:
+    @workflow.run
+    async def run(self, name: str) -> str:
+        return f"Hello, {name} from Temporal!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
